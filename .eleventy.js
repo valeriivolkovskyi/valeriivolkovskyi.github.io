ae4389bd58c68c8a0feca48365f416d3d6b4fbc6
valerii_v <volkovskiy.v@gmail.com>
2024-02-11 15:23:16 +0300

    change dist folder

--- .eleventy.js
+++ .eleventy.js
@@ -12,7 +12,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "dist",
+      output: "_site",
       htmlTemplateEngine: "webc"
     }
   }
