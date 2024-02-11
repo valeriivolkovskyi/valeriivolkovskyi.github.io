12c33da2b9b84a29024ea45275ce4124c31d0f68
valerii_v <volkovskiy.v@gmail.com>
2024-02-11 23:11:29 +0300

    writing layout

--- .eleventy.js
+++ .eleventy.js
@@ -3,9 +3,10 @@ const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy(".nojekyll");
+  // eleventyConfig.addPassthroughCopy("src/styles/index.css");
 
   eleventyConfig.addPlugin(pluginWebc, {
-    components: 'src/components/**/*.webc'
+    components: ['src/components/**/*.webc', 'src/layouts/**/*.webc']
   });
 
 
