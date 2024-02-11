d01ef4507aba78e848ec80f5aad5d6c403994d2a
valerii_v <volkovskiy.v@gmail.com>
2024-02-11 15:15:59 +0300

    bundle css

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,19 @@
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+
+
 module.exports = function(eleventyConfig) {
+  // eleventyConfig.addPassthroughCopy("src/css/bundle.css");
+
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: 'src/components/**/*.webc'
+  });
+
+
   return {
     dir: {
-      input: "./src",
-      output: "./dist"
+      input: "src",
+      output: "dist",
+      htmlTemplateEngine: "webc"
     }
   }
 };
