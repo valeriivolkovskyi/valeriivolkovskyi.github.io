029f6a316c09ba36861323a16c4aaff9b2ea94f2
valerii_v <volkovskiy.v@gmail.com>
2024-02-11 15:27:11 +0300

    add .nojekyll to dist

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 
 module.exports = function(eleventyConfig) {
-  // eleventyConfig.addPassthroughCopy("src/css/bundle.css");
+  eleventyConfig.addPassthroughCopy(".nojekyll");
 
   eleventyConfig.addPlugin(pluginWebc, {
     components: 'src/components/**/*.webc'
