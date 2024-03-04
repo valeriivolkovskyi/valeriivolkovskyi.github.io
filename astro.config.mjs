0e49d14df1ee4af47a5a0bf11b0bf2d05dc641e3
valerii_v <volkovskiy.v@gmail.com>
2024-03-04 23:53:07 +0300

    back to astro

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://walksky.dev',
+	integrations: [sitemap()],
+	output: 'static'
+});
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "WalkSky web development: Where code meets passion";
+export const SITE_DESCRIPTION = 'Passionate development, pragmatic approach to deliver innovative and effective web solutions';
